687370cb8cbaed0f516ea4d207dd23e310630c20
Wesley Chang <weschang15@gmail.com>
2017-12-18 21:20:46 -0800

    Update google strategy to allow proxy and create new api keys

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+
+// require user models for use with mongoose (declared early so that passport can use model)
+require("./models/User");
+// require passport configuration
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+// allow express to make use of cookie sessions with cookie-session middleware and encrypt cookie with key
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 100,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// require google auth routes and pass express server instance to routes
+require("./routes/auth")(app);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,57 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+/**
+ * Serialize the user that is passed to passport and store in session
+ */
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+/**
+ * Return an instance of the User model to passport whose ID is stored in the session is stored in
+ * the DB
+ */
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+/**
+ * Enable google oauth
+ *
+ * Check if user already exists in database, if they do, then return that user to passport otherwise
+ * create a new user with their google ID saved in the DB and return that new user to passport
+ */
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true
+    },
+    (token, refreshToken, profile, done) => {
+      User.findOne({
+        googleId: profile.id
+      }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({
+            googleId: profile.id
+          })
+            .save()
+            .then(newUser => {
+              done(null, newUser);
+            });
+        }
+      });
+    }
+  )
+);
