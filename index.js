0b340dd869e24a6510317aec97a72d1abdca2270
Wesley Chang <weschang15@gmail.com>
2018-01-01 14:09:19 -0800

    Setup express to serve our build directory and index html file whenever a route isn't handled

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 
 // require passport configuration
@@ -11,6 +12,9 @@ mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+// use body-parser to give our routes access to any incoming request body
+app.use(bodyParser.json());
 
 // allow express to make use of cookie sessions with cookie-session middleware and encrypt cookie with key
 app.use(
@@ -25,6 +29,23 @@ app.use(passport.session());
 
 // require google auth routes and pass express server instance to routes
 app.use(require("./routes/auth"));
+// require routes for billing process
+app.use(require("./routes/billing"));
+
+// ensure that express serves our react build bundle in production
+if (process.env.NODE_ENV === "production") {
+  // serve static assests
+  app.use(express.static("client/build"));
+
+  /**
+   *  serve our client-side index.html file if express doesn't find an appropriate route handler for the
+   *  current request
+   */
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
