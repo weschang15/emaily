1269d41168e2a2dca8ce5dea80b2ee1780c60ac9
Wesley Chang <weschang15@gmail.com>
2018-01-01 14:10:57 -0800

    Added new property to user model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  googleId: String
+  googleId: String,
+  credits: {
+    type: Number,
+    default: 0
+  }
 });
 
 module.exports = mongoose.model("user", userSchema);
