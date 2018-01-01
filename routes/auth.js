f8d01c587fdebea450922ee096a035a95a761487
Wesley Chang <weschang15@gmail.com>
2018-01-01 09:12:48 -0800

    Added redirect rule for google login flow and logout

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,28 @@
+const router = require("express").Router();
 const passport = require("passport");
 
-module.exports = app => {
-  app.get(
-    "/auth/google",
-    passport.authenticate("google", {
-      scope: ["profile", "email"]
-    })
-  );
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google"),
+  (req, res) => {
+    res.redirect("/surveys");
+  }
+);
 
-  app.get("/api/logout", (req, res) => {
-    req.logout();
-    res.send(req.user);
-  });
+router.get("/api/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
-  app.get("/api/current_user", (req, res) => {
-    res.send(req.user);
-  });
-};
+router.get("/api/current_user", (req, res) => {
+  res.send(req.user);
+});
+
+module.exports = router;
