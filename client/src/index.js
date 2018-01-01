2038ef3917d8c0e79e096de054d383b2993bf817
Wesley Chang <weschang15@gmail.com>
2018-01-01 09:09:10 -0800

    Setup rendering component with comment explanation of how redux works alongside thunk

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import "materialize-css/dist/css/materialize.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import App from "./components/App";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+/**
+ * The state of entire application is stored in an object called a 'store'.
+ * Instead of changing the state with setState(), an action is required to modify it.
+ *
+ * An action is simply a function that returns an object which describes
+ * what happened to the state
+ *
+ * In order for the action and state to be tied together, a function called a reducer is required.
+ * Reducers simply return the updated state of the app/component.
+ *
+ * We can then combine all of our reducers into a single reducer with combineReducers() which will
+ * represent the state of our entire application
+ */
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
