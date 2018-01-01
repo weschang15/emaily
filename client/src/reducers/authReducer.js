59588fb3eb535f25ed2e5b62d14bda035d2f4848
Wesley Chang <weschang15@gmail.com>
2018-01-01 14:10:36 -0800

    Removed console log

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,7 @@
 import { FETCH_USER } from "../actions/types";
 
+// Produce updated state for auth object
 export default function(state = null, action) {
-  console.log(action);
   switch (action.type) {
     case FETCH_USER:
       return action.payload || false;
