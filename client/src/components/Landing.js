ead9f9a4d721fe62df510c076bdc93b12593a345
Wesley Chang <weschang15@gmail.com>
2018-01-01 09:11:22 -0800

    Componentized landing

---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Collect Feedback From Your Users</h1>
+    </div>
+  );
+};
+
+export { Landing };
