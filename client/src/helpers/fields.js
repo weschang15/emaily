cad92ff23353cca6ff6bf427852c3fd1f817752f
Wesley Chang <weschang15@gmail.com>
2018-04-14 22:24:26 -0700

    Create helper files for form fields and validation

---
+++ client/src/helpers/fields.js
@@ -0,0 +1,26 @@
+export const fields = [
+  {
+    type: "text",
+    name: "title",
+    label: "Survey Title",
+    error: "You must provide a title."
+  },
+  {
+    type: "text",
+    name: "subject",
+    label: "Survey Subject",
+    error: "You must provide a subject."
+  },
+  {
+    type: "text",
+    name: "body",
+    label: "Survey Body",
+    error: "You must provide a body."
+  },
+  {
+    type: "text",
+    name: "recipients",
+    label: "Survey Recipients",
+    error: "You must provide a list of recipients."
+  }
+];
