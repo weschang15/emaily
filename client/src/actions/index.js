9d73ef144afa07dc6b6b943addf71d31190d9e34
Wesley Chang <weschang15@gmail.com>
2018-04-14 22:22:35 -0700

    Create redux action that handles survey send request and action dispatch

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -12,6 +12,22 @@ export const fetchUser = () => async dispatch => {
 };
 
 /**
+ * Action that fires a post request to our backend route for creating a survey, sending the survey,
+ * and updating our current user's token count.
+ *
+ * * Handle the redirect to /surveys after the request to send survey has been successfully fired
+ *
+ * @param {obj} values contains our submitted SurveyForm values
+ * @param {obj} history allow react router to redirect to another component after request has been made
+ */
+export const submitSurvey = (values, history) => async dispatch => {
+  const res = await axios.post("/api/surveys", values);
+
+  history.push("/surveys");
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+/**
  * An action creator that sends the token returned from Stripe to our backend server and dispatch the action
  * only when the token has been sent to our backend route.
  *
